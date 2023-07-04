77f3100bbdb2a56fd0ecaedc0c6736abe3f43379
Sharpx <sharpx-@outlook.com>
2023-07-04 10:48:14 +0800

    feat: add topic
    Add chaincode for topic with unit tests

---
+++ topic/chaincode/smartcontract.go
@@ -0,0 +1,214 @@
+package chaincode
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type Topic struct {
+	Id         string    `json:"id"`
+	CId        string    `json:"cid"`
+	Creator    string    `json:"creator"`
+	CreateTime time.Time `json:"createTime"`
+	UpdateTime time.Time `json:"updateTime"`
+	Category   string    `json:"category"`
+	Tags       []string  `json:"tags"`
+}
+
+const TimeFormat = "2006-01-02 15:04:05" // deprecated: use time.Time instead of string
+
+// InitLedger adds a base set of topics to the ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	txntmsp, _ := ctx.GetStub().GetTxTimestamp()
+	timestamp := txntmsp.AsTime()
+	topics := []Topic{
+		{Id: "1", CId: "c1", Creator: "user1", CreateTime: timestamp, UpdateTime: timestamp, Category: "category1", Tags: []string{"tag1", "tag2"}},
+		{Id: "2", CId: "c2", Creator: "user2", CreateTime: timestamp, UpdateTime: timestamp, Category: "category2", Tags: []string{"tag1", "tag2", "tag3"}},
+		{Id: "3", CId: "c3", Creator: "user3", CreateTime: timestamp, UpdateTime: timestamp, Category: "category3", Tags: []string{"tag1", "tag2", "tag3", "tag4"}},
+	}
+
+	for _, topic := range topics {
+		assetJSON, _ := json.Marshal(topic)
+
+		err := ctx.GetStub().PutState(topic.Id, assetJSON)
+		if err != nil {
+			return fmt.Errorf("failed to put to world state. %v", err)
+		}
+	}
+
+	return nil
+}
+
+// Get client identity which submit the transaction
+func (s *SmartContract) GetSubmittingClientIdentity(ctx contractapi.TransactionContextInterface) (string, error) {
+
+	b64ID, err := ctx.GetClientIdentity().GetID()
+	if err != nil {
+		return "", fmt.Errorf("failed to read clientID: %v", err)
+	}
+	decodeID, err := base64.StdEncoding.DecodeString(b64ID)
+	if err != nil {
+		return "", fmt.Errorf("failed to base64 decode clientID: %v", err)
+	}
+	return string(decodeID), nil
+}
+
+// CreateTopic creates a topic.
+func (s *SmartContract) CreateTopic(ctx contractapi.TransactionContextInterface, topicId string, cid string, category string, tags ...string) error {
+	operator, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	exists, err := s.TopicExists(ctx, topicId)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the topic %s already exists", topicId)
+	}
+
+	txntmsp, _ := ctx.GetStub().GetTxTimestamp()
+	timestamp := txntmsp.AsTime()
+	topic := Topic{
+		Id:         topicId,
+		CId:        cid,
+		Creator:    operator,
+		CreateTime: timestamp,
+		UpdateTime: timestamp,
+		Category:   category,
+		Tags:       tags,
+	}
+
+	topicJSON, _ := json.Marshal(topic)
+	return ctx.GetStub().PutState(topicId, topicJSON)
+}
+
+// TopicExists returns true when topic with given ID exists in world state
+func (s *SmartContract) TopicExists(ctx contractapi.TransactionContextInterface, topicId string) (bool, error) {
+	topicJSON, err := ctx.GetStub().GetState(topicId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return topicJSON != nil, nil
+}
+
+// ReadTopic returns the topic stored in the world state with given id.
+func (s *SmartContract) ReadTopic(ctx contractapi.TransactionContextInterface, topicId string) (*Topic, error) {
+	topicJSON, err := ctx.GetStub().GetState(topicId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if topicJSON == nil {
+		return nil, fmt.Errorf("the topic %s does not exist", topicId)
+	}
+
+	var topic Topic
+	json.Unmarshal(topicJSON, &topic)
+
+	return &topic, nil
+}
+
+// UpdateTopic updates an existing topic in the world state with provided parameters.
+func (s *SmartContract) UpdateTopic(ctx contractapi.TransactionContextInterface, topicId string, cid string, category string, tags ...string) error {
+	topic, err := s.ReadTopic(ctx, topicId)
+	if err != nil {
+		return err
+	}
+
+	operator, err := s.GetSubmittingClientIdentity(ctx)
+	if err != nil {
+		return err
+	}
+
+	if topic.Creator != operator {
+		return fmt.Errorf("the topic %s can only be updated by its creator", topicId)
+	}
+
+	txntmsp, _ := ctx.GetStub().GetTxTimestamp()
+	timestamp := txntmsp.AsTime()
+
+	topic.CId = cid
+	topic.UpdateTime = timestamp
+	topic.Category = category
+	topic.Tags = tags
+	topicJSON, _ := json.Marshal(topic)
+
+	return ctx.GetStub().PutState(topicId, topicJSON)
+}
+
+// GetAllTopics returns all topics found in world state
+func (s *SmartContract) GetAllTopics(ctx contractapi.TransactionContextInterface) ([]*Topic, error) {
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var topics []*Topic
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var topic Topic
+		json.Unmarshal(queryResponse.Value, &topic)
+		topics = append(topics, &topic)
+	}
+
+	return topics, nil
+}
+
+func (s *SmartContract) QueryTopicsByCreator(ctx contractapi.TransactionContextInterface, creator string) ([]*Topic, error) {
+	queryString := fmt.Sprintf(`{"selector":{"creator":"%s"}}`, creator)
+	return getQueryResultForQueryString(ctx, queryString)
+}
+
+func (s *SmartContract) QueryTopicsByCategory(ctx contractapi.TransactionContextInterface, category string) ([]*Topic, error) {
+	queryString := fmt.Sprintf(`{"selector":{"category":"%s"}}`, category)
+	return getQueryResultForQueryString(ctx, queryString)
+}
+
+func (s *SmartContract) QueryTopicsByTag(ctx contractapi.TransactionContextInterface, tag string) ([]*Topic, error) {
+	queryString := fmt.Sprintf(`{"selector":{"tags":{"$elemMatch":{"$eq":"%s"}}}}`, tag)
+	return getQueryResultForQueryString(ctx, queryString)
+}
+
+// getQueryResultForQueryString executes the passed in query string.
+// The result set is built and returned as a byte array containing the JSON results.
+func getQueryResultForQueryString(ctx contractapi.TransactionContextInterface, queryString string) ([]*Topic, error) {
+	resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	return constructQueryResponseFromIterator(resultsIterator)
+}
+
+// constructQueryResponseFromIterator constructs a slice of topics from the resultsIterator
+func constructQueryResponseFromIterator(resultsIterator shim.StateQueryIteratorInterface) ([]*Topic, error) {
+	var topics []*Topic
+	for resultsIterator.HasNext() {
+		queryResult, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		var topic Topic
+		json.Unmarshal(queryResult.Value, &topic)
+		topics = append(topics, &topic)
+	}
+
+	return topics, nil
+}
---
+++ topic/chaincode/smartcontract_test.go
@@ -0,0 +1,271 @@
+package chaincode_test
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"topic/chaincode"
+	"topic/chaincode/mocks"
+
+	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+
+	// _ "github.com/maxbrunsfeld/counterfeiter/v6"
+	"github.com/hyperledger/fabric-protos-go/ledger/queryresult"
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/transaction.go -fake-name TransactionContext . transactionContext
+type transactionContext interface {
+	contractapi.TransactionContextInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/chaincodestub.go -fake-name ChaincodeStub . chaincodeStub
+type chaincodeStub interface {
+	shim.ChaincodeStubInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/statequeryiterator.go -fake-name StateQueryIterator . stateQueryIterator
+type stateQueryIterator interface {
+	shim.StateQueryIteratorInterface
+}
+
+//go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 -o mocks/clientIdentity.go -fake-name ClientIdentity . clientIdentity
+type clientIdentity interface {
+	cid.ClientIdentity
+}
+
+const myOrg1Msp = "Org1Testmsp"
+const myOrg1Clientid = "myOrg1Userid"
+const myOrg1PrivCollection = "Org1TestmspPrivateCollection"
+const myOrg2Msp = "Org2Testmsp"
+const myOrg2Clientid = "myOrg2Userid"
+const myOrg2PrivCollection = "Org2TestmspPrivateCollection"
+
+func TestInitLedger(t *testing.T) {
+	chaincodeStub := &mocks.ChaincodeStub{}
+	transactionContext := &mocks.TransactionContext{}
+	transactionContext.GetStubReturns(chaincodeStub)
+
+	topic := chaincode.SmartContract{}
+	err := topic.InitLedger(transactionContext)
+	require.NoError(t, err)
+
+	chaincodeStub.PutStateReturns(fmt.Errorf("failed inserting key"))
+	err = topic.InitLedger(transactionContext)
+	require.EqualError(t, err, "failed to put to world state. failed inserting key")
+}
+
+func TestGetSubmittingClientIdentity(t *testing.T) {
+	transactionContext, _ := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	_, err := topic.GetSubmittingClientIdentity(transactionContext)
+	require.NoError(t, err)
+
+	tr := &mocks.TransactionContext{}
+	clientIdentity := &mocks.ClientIdentity{}
+	clientIdentity.GetIDReturns("", fmt.Errorf("failure"))
+	tr.GetClientIdentityReturns(clientIdentity)
+	_, err = topic.GetSubmittingClientIdentity(tr)
+	require.EqualError(t, err, "failed to read clientID: failure")
+	clientIdentity.GetIDReturns("aa", nil)
+	_, err = topic.GetSubmittingClientIdentity(tr)
+	require.EqualError(t, err, "failed to base64 decode clientID: illegal base64 data at input byte 0")
+}
+
+func TestCreateTopic(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	err := topic.CreateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.NoError(t, err)
+
+	chaincodeStub.GetStateReturns([]byte{}, fmt.Errorf("failure"))
+	err = topic.CreateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "failed to read from world state: failure")
+
+	expectedTopic := &chaincode.Topic{Id: "1"}
+	bytes, err := json.Marshal(expectedTopic)
+	require.NoError(t, err)
+
+	chaincodeStub.GetStateReturns(bytes, nil)
+	err = topic.CreateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "the topic 1 already exists")
+
+	transactionContext, _ = prepMocksIllegalId()
+	err = topic.CreateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "failed to read clientID: failure")
+}
+
+func TestReadTopic(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	tmpTopic := &chaincode.Topic{Id: "1"}
+	bytes, _ := json.Marshal(tmpTopic)
+	chaincodeStub.GetStateReturns(bytes, nil)
+	_, err := topic.ReadTopic(transactionContext, "1")
+	require.NoError(t, err)
+
+	chaincodeStub.GetStateReturns([]byte{}, fmt.Errorf("failure"))
+	_, err = topic.ReadTopic(transactionContext, "1")
+	require.EqualError(t, err, "failed to read from world state: failure")
+
+	chaincodeStub.GetStateReturns(nil, nil)
+	_, err = topic.ReadTopic(transactionContext, "1")
+	require.EqualError(t, err, "the topic 1 does not exist")
+}
+
+func TestUpdateTopic(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	err := topic.UpdateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "the topic 1 does not exist")
+
+	chaincodeStub.GetStateReturns([]byte{}, fmt.Errorf("failure"))
+	err = topic.UpdateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "failed to read from world state: failure")
+
+	tmpTopic := &chaincode.Topic{Id: "1", Creator: myOrg1Clientid}
+	bytes, _ := json.Marshal(tmpTopic)
+	chaincodeStub.GetStateReturns(bytes, nil)
+
+	err = topic.UpdateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.NoError(t, err)
+
+	tmpTopic = &chaincode.Topic{Id: "1", Creator: myOrg2Clientid}
+	bytes, _ = json.Marshal(tmpTopic)
+	chaincodeStub.GetStateReturns(bytes, nil)
+
+	err = topic.UpdateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "the topic 1 can only be updated by its creator")
+
+	transactionContext, chaincodeStub = prepMocksIllegalId()
+	tmpTopic = &chaincode.Topic{Id: "1"}
+	bytes, _ = json.Marshal(tmpTopic)
+	chaincodeStub.GetStateReturns(bytes, nil)
+
+	err = topic.UpdateTopic(transactionContext, "1", "1", "1", "1", "1", "1", "1", "1", "1")
+	require.EqualError(t, err, "failed to read clientID: failure")
+}
+
+func TestGetAllTopics(t *testing.T) {
+	asset := &chaincode.Topic{Id: "user1"}
+	bytes, err := json.Marshal(asset)
+	require.NoError(t, err)
+
+	iterator := &mocks.StateQueryIterator{}
+	iterator.HasNextReturnsOnCall(0, true)
+	iterator.HasNextReturnsOnCall(1, false)
+	iterator.NextReturns(&queryresult.KV{Value: bytes}, nil)
+
+	chaincodeStub := &mocks.ChaincodeStub{}
+	transactionContext := &mocks.TransactionContext{}
+	transactionContext.GetStubReturns(chaincodeStub)
+
+	chaincodeStub.GetStateByRangeReturns(iterator, nil)
+	userprofile := &chaincode.SmartContract{}
+	assets, err := userprofile.GetAllTopics(transactionContext)
+	require.NoError(t, err)
+	require.Equal(t, []*chaincode.Topic{asset}, assets)
+
+	iterator.HasNextReturns(true)
+	iterator.NextReturns(nil, fmt.Errorf("failed retrieving next item"))
+	assets, err = userprofile.GetAllTopics(transactionContext)
+	require.EqualError(t, err, "failed retrieving next item")
+	require.Nil(t, assets)
+
+	chaincodeStub.GetStateByRangeReturns(nil, fmt.Errorf("failed retrieving all assets"))
+	assets, err = userprofile.GetAllTopics(transactionContext)
+	require.EqualError(t, err, "failed retrieving all assets")
+	require.Nil(t, assets)
+}
+
+func TestQueryTopicsByCreator(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	chaincodeStub.GetQueryResultReturns(nil, fmt.Errorf("failure"))
+	_, err := topic.QueryTopicsByCreator(transactionContext, "1")
+	require.EqualError(t, err, "failure")
+
+	tmpTopic := &chaincode.Topic{Id: "user1", Creator: myOrg1Clientid}
+	bytes, _ := json.Marshal(tmpTopic)
+
+	iterator := &mocks.StateQueryIterator{}
+	iterator.HasNextReturnsOnCall(0, true)
+	iterator.HasNextReturnsOnCall(1, false)
+	iterator.NextReturns(&queryresult.KV{Value: bytes}, nil)
+
+	chaincodeStub.GetQueryResultReturns(iterator, nil)
+	topics, err := topic.QueryTopicsByCreator(transactionContext, "1")
+	require.NoError(t, err)
+	require.Equal(t, []*chaincode.Topic{tmpTopic}, topics)
+
+	iterator.NextReturns(nil, fmt.Errorf("failed retrieving next item"))
+	iterator.HasNextReturnsOnCall(2, true)
+	iterator.HasNextReturnsOnCall(3, true)
+	chaincodeStub.GetQueryResultReturns(iterator, nil)
+	topics, err = topic.QueryTopicsByCreator(transactionContext, "1")
+	require.EqualError(t, err, "failed retrieving next item")
+	require.Nil(t, topics)
+}
+
+func TestQueryTopicsByCategory(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	chaincodeStub.GetQueryResultReturns(nil, fmt.Errorf("failure"))
+	_, err := topic.QueryTopicsByCategory(transactionContext, "1")
+	require.EqualError(t, err, "failure")
+}
+
+func TestQueryTopicsByTag(t *testing.T) {
+	transactionContext, chaincodeStub := prepMocksAsOrg1()
+	topic := chaincode.SmartContract{}
+
+	chaincodeStub.GetQueryResultReturns(nil, fmt.Errorf("failure"))
+	_, err := topic.QueryTopicsByTag(transactionContext, "1")
+	require.EqualError(t, err, "failure")
+}
+
+func prepMocksAsOrg1() (*mocks.TransactionContext, *mocks.ChaincodeStub) {
+	return prepMocks(myOrg1Msp, myOrg1Clientid)
+}
+func prepMocksAsOrg2() (*mocks.TransactionContext, *mocks.ChaincodeStub) {
+	return prepMocks(myOrg2Msp, myOrg2Clientid)
+}
+func prepMocks(orgMSP, clientId string) (*mocks.TransactionContext, *mocks.ChaincodeStub) {
+	chaincodeStub := &mocks.ChaincodeStub{}
+	transactionContext := &mocks.TransactionContext{}
+	transactionContext.GetStubReturns(chaincodeStub)
+
+	clientIdentity := &mocks.ClientIdentity{}
+	clientIdentity.GetMSPIDReturns(orgMSP, nil)
+	clientIdentity.GetIDReturns(base64.StdEncoding.EncodeToString([]byte(clientId)), nil)
+	// set matching msp ID using peer shim env variable
+	os.Setenv("CORE_PEER_LOCALMSPID", orgMSP)
+	transactionContext.GetClientIdentityReturns(clientIdentity)
+	return transactionContext, chaincodeStub
+}
+
+func prepMocksIllegalId() (*mocks.TransactionContext, *mocks.ChaincodeStub) {
+	chaincodeStub := &mocks.ChaincodeStub{}
+	transactionContext := &mocks.TransactionContext{}
+	transactionContext.GetStubReturns(chaincodeStub)
+
+	clientIdentity := &mocks.ClientIdentity{}
+	clientIdentity.GetMSPIDReturns(myOrg1Msp, nil)
+	// clientIdentity.GetIDReturns("illegal", nil)
+	clientIdentity.GetIDReturns("", fmt.Errorf("failure"))
+	// set matching msp ID using peer shim env variable
+	os.Setenv("CORE_PEER_LOCALMSPID", myOrg1Msp)
+	transactionContext.GetClientIdentityReturns(clientIdentity)
+	return transactionContext, chaincodeStub
+}
