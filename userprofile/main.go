4b2e84829ce77f7c4d371f78040e403a2375e63e
Yiyang Wu <toolmanp@outlook.com>
2023-09-08 19:20:25 +0800

    feat(userprofile): use userprofile external services
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

---
+++ userprofile/main.go
@@ -0,0 +1,48 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+	"os"
+
+	"userprofile/chaincode"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+func main() {
+
+	type serverConfig struct {
+		CCID    string
+		Address string
+	}
+
+	chaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+
+	if err != nil {
+		log.Panicf("Error creating userprofile chaincode: %v", err)
+	}
+
+	config := serverConfig{
+		CCID:    os.Getenv("CHAINCODE_ID"),
+		Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
+	}
+
+	server := &shim.ChaincodeServer{
+		CCID:    config.CCID,
+		Address: config.Address,
+		CC:      chaincode,
+		TLSProps: shim.TLSProperties{
+			Disabled: true,
+		},
+	}
+
+	if err := server.Start(); err != nil {
+		log.Panicf("error starting userprofile chaincode server: %s", err)
+	}
+
+}
